013f998e2849daaf5ae2f2a006e9399595a7c7e4
Lorenzo Martin <alu0100537116@ull.edu.es>
2013-10-17 19:10:14 +0100

    OPERACIONES Y TEST TERMINADOS

--- racional.rb
+++ racional.rb
@@ -22,41 +22,53 @@ class Fraccion
     com_div = gcd(@num,@den)
     @num=@num/com_div
     @den=@den/com_div
-    #Fraccion.new(@num/com_div,@den/com_div)
+    Fraccion.new(@num,@den)
   end
   
   def suma (numerador,denominador)
       if denominador == @den
 	  aux=numerador+@num
-	  puts " Denominadores iguales "
 	  p3=Fraccion.new(aux, denominador)
       else
-	  puts " Denominadores diferentes "
 	  numaux=(@num*denominador)+(numerador*@den)
 	  denaux=@den*denominador
 	  p3=Fraccion.new(numaux, denaux)
       end 
     p3.reduccion
-    puts p3.to_s
    end
    
   def resta (numerador,denominador)
     if denominador == @den
 	  aux=numerador-@num
-	  puts " Denominadores iguales "
 	  p3=Fraccion.new(aux, denominador)
       else
-	  puts " Denominadores diferentes "
 	  numaux=(@num*denominador)-(numerador*@den)
 	  denaux=@den*denominador
 	  p3=Fraccion.new(numaux, denaux)
       end 
     p3.reduccion
-    puts p3.to_s
+    
   end
     
+  def producto(numerador,denominador)
+    
+    aux1=@num*numerador
+    aux2=@den*denominador
+    p3=Fraccion.new(aux1,aux2)
+    p3.reduccion
+    
+  end
+  
+  def division(numerador, denominador)
+    aux1=@num*denominador
+    aux2=@den*numerador
+    p3=Fraccion.new(aux1,aux2)
+    p3.reduccion
+  end
 end
 
-
-p1=Fraccion.new(3,5).suma(3,3) # SUMA (3/3 + 3/5)
-p2=Fraccion.new(2,2).resta(4,2) # RESTA (4/2 - 2/2)
+# 
+# puts Fraccion.new(3,5).suma(3,3) # SUMA (3/3 + 3/5)
+# puts Fraccion.new(2,2).resta(4,2) # RESTA (4/2 - 2/2)
+# puts Fraccion.new(4,2).producto(3,3) # MULTIPLICA (2/2 * 3/3)
+# puts Fraccion.new(2,2).division(4,2) # DIVIDE (/2) / (2/2)
